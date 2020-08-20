956528fed5e53a19a480b41ab2c6b032ffe700f0
seveibar <seveibar@gmail.com>
2020-08-19 21:40:33 -0400

    added video to homepage

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -145,6 +145,16 @@ const LandingPage = () => {
       </Hero>
       <ContentContainer className="markdown-body">
         <Section className="markdown-body">
+          <Box textAlign="center">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/2XXENmAIyzE"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </Box>
           <h1>Features</h1>
           <p>
             <ul>
